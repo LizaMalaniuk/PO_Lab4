6187eeaaf26a777ca2479fa9a64075177d415e95
Liza Malaniuk <lizamal270605@gmail.com>
2025-04-27 03:30:43 +0300

    Базові функції обміну повідомленнями

--- client.cpp
+++ client.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <winsock2.h>
 #include <ws2tcpip.h>
+#include <vector>
+#include <cstdint>
 
 #pragma comment(lib, "Ws2_32.lib")
 
@@ -8,6 +10,33 @@ using namespace std;
 
 const int PORT = 12345;
 const char* SERVER_IP = "127.0.0.1";
+
+int recvAll(SOCKET sock, char* buffer, int length) {
+    int received = 0;
+    while (received < length) {
+        int r = recv(sock, buffer + received, length - received, 0);
+        if (r <= 0) return -1;
+        received += r;
+    }
+    return received;
+}
+
+int recvMessage(SOCKET sock, vector<char>& data) {
+    uint32_t length_be;
+    if (recvAll(sock, reinterpret_cast<char*>(&length_be), 4) != 4) return -1;
+
+    uint32_t length = ntohl(length_be);
+    if (length > 1e7) return -1;
+
+    data.resize(length);
+    return recvAll(sock, data.data(), length);
+}
+
+bool sendMessage(SOCKET sock, const vector<char>& data) {
+    uint32_t len = htonl(data.size());
+    if (send(sock, reinterpret_cast<const char*>(&len), 4, 0) != 4) return false;
+    return send(sock, data.data(), data.size(), 0) == data.size();
+}
 
 int main() {
     WSADATA wsa;
@@ -25,6 +54,11 @@ int main() {
     }
 
     cout << "[CLIENT] Connected to server.\n";
+    vector<char> response;
+    if (recvMessage(sock, response) > 0) {
+        cout << "[CLIENT] Received message: " << string(response.begin(), response.end()) << endl;
+    }
+
 
     closesocket(sock);
     WSACleanup();
--- server.cpp
+++ server.cpp
@@ -1,12 +1,41 @@
 #include <iostream>
 #include <winsock2.h>
 #include <ws2tcpip.h>
+#include <vector>
+#include <cstdint>
 
 #pragma comment(lib, "Ws2_32.lib")
 
 using namespace std;
 
 const int PORT = 12345;
+
+int recvAll(SOCKET sock, char* buffer, int length) {
+    int received = 0;
+    while (received < length) {
+        int r = recv(sock, buffer + received, length - received, 0);
+        if (r <= 0) return -1;
+        received += r;
+    }
+    return received;
+}
+
+int recvMessage(SOCKET sock, vector<char>& data) {
+    uint32_t length_be;
+    if (recvAll(sock, reinterpret_cast<char*>(&length_be), 4) != 4) return -1;
+
+    uint32_t length = ntohl(length_be);
+    if (length > 1e7) return -1;
+
+    data.resize(length);
+    return recvAll(sock, data.data(), length);
+}
+
+bool sendMessage(SOCKET sock, const vector<char>& data) {
+    uint32_t len = htonl(data.size());
+    if (send(sock, reinterpret_cast<const char*>(&len), 4, 0) != 4) return false;
+    return send(sock, data.data(), data.size(), 0) == data.size();
+}
 
 int main() {
     WSADATA wsa;
@@ -25,6 +54,10 @@ int main() {
 
     SOCKET clientSock = accept(serverSock, nullptr, nullptr);
     cout << "[SERVER] Client accepted.\n";
+
+    vector<char> message{'H', 'e', 'l', 'l', 'o'};
+    sendMessage(clientSock, message);
+
 
     closesocket(clientSock);
     closesocket(serverSock);
